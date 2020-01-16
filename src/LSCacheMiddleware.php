8c80ee4cdeabf6a1bb336b69e339fd0838140943
Madalin Tache <madalin@madalin.eu>
2020-01-16 13:28:47 +0200

    Allow fallback when checking for guests only, and document it

--- src/LSCacheMiddleware.php
+++ src/LSCacheMiddleware.php
@@ -3,37 +3,37 @@
 namespace Litespeed\LSCache;
 
 use Closure;
-use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 
 class LSCacheMiddleware
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @param  string $lscache_control
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure                 $next
+     * @param  string                   $lscache_control
      * @return mixed
      */
     public function handle($request, Closure $next, string $lscache_control = null)
     {
         $response = $next($request);
 
-        if (! $request->isMethodCacheable() || ! $response->getContent()) {
+        if ( ! $request->isMethodCacheable() || ! $response->getContent()) {
             return $response;
         }
 
         $esi_enabled    = config('lscache.esi');
         $maxage         = config('lscache.default_ttl');
         $cacheability   = config('lscache.default_cacheability');
-        $guest_only     = config('lscache.guest_only');
+        $guest_only     = config('lscache.guest_only', false);
 
-        if($maxage === 0 && $lscache_control === null) {
+        if ($maxage === 0 && $lscache_control === null) {
             return $response;
         }
 
-        if($guest_only == true && Auth::check() == true) {
+        if ($guest_only && Auth::check()) {
             $response->headers->set('X-LiteSpeed-Cache-Control', 'no-cache');
 
             return $response;
@@ -41,15 +41,15 @@ class LSCacheMiddleware
 
         $lscache_string = "max-age=$maxage,$cacheability";
 
-        if(isset($lscache_control)) {
+        if (isset($lscache_control)) {
             $lscache_string = str_replace(';', ',', $lscache_control);
         }
 
-        if(Str::contains($lscache_string, 'esi=on') == false) {
-            $lscache_string = $lscache_string  . ($esi_enabled ? ',esi=on' : null);
+        if (Str::contains($lscache_string, 'esi=on') == false) {
+            $lscache_string = $lscache_string.($esi_enabled ? ',esi=on' : null);
         }
 
-        if($response->headers->has('X-LiteSpeed-Cache-Control') == false) {
+        if ($response->headers->has('X-LiteSpeed-Cache-Control') == false) {
             $response->headers->set('X-LiteSpeed-Cache-Control', $lscache_string);
         }
 
